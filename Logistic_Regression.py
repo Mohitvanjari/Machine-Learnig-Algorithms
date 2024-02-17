cf1e356bdb5d1b01cee2ba56c76beefa7e7c1688
Mohitvanjari <mohitvanjari6476@gmail.com>
2024-02-17 10:42:26 +0530

    Add files via upload

---
+++ Logistic_Regression.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+
+#sample dataset
+credit_scores = np.random.randint(300, 850, size=1000)
+loan_approved = np.random.binomial(1, p=1 / (1 + np.exp(-0.02 * (credit_scores - 700))), size=1000)
+
+data = pd.DataFrame({
+    'credit_score': credit_scores,
+    'loan_approved': loan_approved
+})
+
+print(data.head())
+
+from sklearn.linear_model import LogisticRegression
+
+model = LogisticRegression()
+model.fit(data[['credit_score']], data['loan_approved'])
+
+new_customer_credit_score = 750
+new_customer_data = pd.DataFrame({'credit_score': [new_customer_credit_score]})
+predicted_class = model.predict(new_customer_data[['credit_score']])[0]
+
+# print the predicted class
+if predicted_class == 0:
+    print(f'A loan is not approved for a customer with a credit score of {new_customer_credit_score}.')
+else:
+    print(f'A loan is approved for a customer with a credit score of {new_customer_credit_score}.')
